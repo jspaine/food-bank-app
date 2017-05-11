e2e2d0b249ba82574b9831570a62fc8602d7651d
Kenji Okamoto <kenjio@users.noreply.github.com>
2017-05-11 09:01:31 -0700

    Make the secrets file available in all environments so tests will work

--- server/config/env/all.js
+++ server/config/env/all.js
@@ -1,3 +1,12 @@
+let secrets
+
+try {
+  secrets = require('./secrets').default
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.log('No secrets file found - copy server/config/env/secrets-template.js to server/config/env/secrets.js to add keys for development')
+}
+
 module.exports = {
   protocol: process.env.PROTOCOL || 'http',
   host: process.env.HOST || 'localhost',
@@ -8,5 +17,6 @@ module.exports = {
     from: process.env.MAILER_FROM || 'MAILER_FROM',
     to: process.env.MAILER_TO || 'MAILER_TO',
     sendgridKey: process.env.SENDGRID_API_KEY || 'SEND_GRID_API_KEY'
-  }
+  },
+  ...secrets
 }
