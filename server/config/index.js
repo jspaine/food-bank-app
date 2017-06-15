e84a7adf9dd4a3a281d14e3856f5c41b2b84c75d
jspaine <painejs@hotmail.com>
2017-06-16 00:52:31 +0200

    allow passing secrets as env vars

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,9 @@
-import {extend} from 'lodash'
+import {merge} from 'lodash'
 
 const env = process.env.NODE_ENV || 'development'
 
-export default extend(
+export default merge(
   require('./env/all').default,
-  require(`./env/${env}`).default
+  require(`./env/${env}`).default,
+  require('./env/secrets').default
 )
